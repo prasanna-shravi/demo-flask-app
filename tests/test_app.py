4a7dc97ea934fcea08cc8ac3c6953ac125b0edba
prasanna-shravi <99714632+prasanna-shravi@users.noreply.github.com>
2022-04-01 21:52:39 +0100

    Changes for Demo

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,4 +7,4 @@ def test_flaskapp():
 
     res = requests.get("http://localhost:5000/")
     assert res.status_code == 200
-    assert res.content == b'Automatically deployed and tested.'
+    assert res.content == b'Automatically deployed and tested using Jenkins.'
