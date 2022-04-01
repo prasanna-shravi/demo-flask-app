b9f108a2f5103a1689fe8702102aeb89a2b1882e
prasanna-shravi <99714632+prasanna-shravi@users.noreply.github.com>
2022-04-01 16:46:49 +0100

    Changes for Demo 1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return 'Automatically deployed and tested using Github-Jenkins-Docker.'
+    return 'Automatically deployed and tested.'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
